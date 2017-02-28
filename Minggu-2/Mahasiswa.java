a197f605acd66942ffe5081da84aaa11a2538418
Fajar Abdi Nugraha <nugrahafajar37@gmail.com>
2017-02-28 22:07:14 +0700

    Add files via upload

---
+++ Minggu-2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+	public static void main(String args[]) {
+		System.out.println("Nama Lengkap : Fajar Abdi Nugraha");
+		System.out.println("NIM          : 10115088");
+		System.out.println("Alamat       : Bandung");
+	}
+}
