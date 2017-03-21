65efc60f479d59352842cda409590daf4d51373c
Fajar Abdi Nugraha <nugrahafajar37@gmail.com>
2017-03-21 23:24:12 +0700

    Add files via upload

---
+++ Minggu-5/Anjing.java
@@ -0,0 +1,21 @@
+/*
+   NIM  : 10115088
+   Nama : Fajar Abdi Nugraha
+*/
+
+public class Anjing extends Mamalia {
+	
+	public boolean taring;
+	public boolean cakar;
+	
+	public void menggonggong() {
+		
+	}
+	public void mengigit() {
+		
+	}
+	public void berlari() {
+		
+	}
+
+}
---
+++ Minggu-5/Mamalia.java
@@ -0,0 +1,20 @@
+/*
+   NIM  : 10115088
+   Nama : Fajar Abdi Nugraha
+*/
+
+public class Mamalia {
+	
+	public boolean kelenjarSusu;
+	public boolean rambut;
+	public boolean paruParu;
+	public int ruangJantung;
+	
+	public void menyusui() {
+		
+	}
+	public void Melahirkan() {
+		
+	}
+
+}
