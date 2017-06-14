c71d62e103fee5ac9dc55b030b3a4e74ef975f70
Fajar Abdi Nugraha <nugrahafajar37@gmail.com>
2017-06-14 23:20:27 +0700

    Add files via upload

---
+++ Minggu 14 - Package/LatihanPackage/src/Buku/dataBuku.java
@@ -0,0 +1,34 @@
+package Buku;
+
+/*
+ * NIM	 : 10115088
+ * Nama  : Fajar Abdi Nugraha
+ * Kelas : ProLan - 12
+ * Tugas : Generic class
+ */
+
+public class dataBuku<T, N> {
+	
+	private T  jdlBuku;
+	private N jmlBuku, noBuku;
+
+	public dataBuku(N noBuku, T jdlBuku, N jmlBuku){
+		this.noBuku  = noBuku;
+		this.jdlBuku = jdlBuku;
+		this.jmlBuku = jmlBuku;
+	}
+	
+	public N getNoBuku() {
+		return noBuku;
+	}
+
+	public T getJdlBuku() {
+		return jdlBuku;
+	}
+	
+	public N getJmlBuku() {
+		return jmlBuku;
+	}
+	
+}
+
